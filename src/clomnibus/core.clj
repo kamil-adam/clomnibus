749ef1edef00581a010305d3ede01af377f1f90b
Kamil Zabinski <kamil.zabinski@relayr.io>
2018-05-28 09:45:15 +0200

    fix data

--- src/clomnibus/core.clj
+++ src/clomnibus/core.clj
@@ -1,27 +1,12 @@
 (ns clomnibus.core
-  (:gen-class))
-
-(require '[clojure.data.csv :as csv]
-         '[clojure.java.io :as io])
-
-(defn data-io []
-  (with-open [reader (io/reader "in-file.csv")]
-    (doall
-      (csv/read-csv reader)))
-
-  (with-open [writer (io/writer "out-file.csv")]
-    (csv/write-csv writer
-                   [["abc" "def"]
-                    ["ghi" "jkl"]]))
-  )
-
+  (:gen-class)
+  (require [clomnibus.data :refer :all])
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (data-io)
-  (println "Hello, World!"))
-
-(require '[clojure.data.csv :as csv]
-         '[clojure.java.io :as io])
+  (println "Hello, World!")
+)
 
--- src/clomnibus/data.clj
+++ src/clomnibus/data.clj
@@ -1,7 +1,11 @@
+(ns clomnibus.data
+  (:gen-class)
+)
+
 (require '[clojure.data.csv :as csv]
          '[clojure.java.io :as io])
 
-(defn data-io
+(defn data-io []
 (with-open [reader (io/reader "in-file.csv")]
   (doall
     (csv/read-csv reader)))

33e5d66360ae8ecd02ea5797316a3a457573098d
Kamil Zabinski <kamil.zabinski@relayr.io>
2018-05-28 08:43:25 +0200

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clomnibus
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clomnibus/core_test.clj
@@ -0,0 +1,7 @@
+(ns clomnibus.core-test
+  (:require [clojure.test :refer :all]
+            [clomnibus.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

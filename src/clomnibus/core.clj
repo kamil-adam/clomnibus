485b413a4dfdc1c2c4bc1de0fd58f71155b0e87f
Kamil Zabinski <kamil.zabinski@relayr.io>
2018-05-28 09:39:36 +0200

    add data

--- src/clomnibus/core.clj
+++ src/clomnibus/core.clj
@@ -1,7 +1,27 @@
 (ns clomnibus.core
   (:gen-class))
 
+(require '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io])
+
+(defn data-io []
+  (with-open [reader (io/reader "in-file.csv")]
+    (doall
+      (csv/read-csv reader)))
+
+  (with-open [writer (io/writer "out-file.csv")]
+    (csv/write-csv writer
+                   [["abc" "def"]
+                    ["ghi" "jkl"]]))
+  )
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (data-io)
   (println "Hello, World!"))
+
+(require '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io])
+
---
+++ src/clomnibus/data.clj
@@ -0,0 +1,13 @@
+(require '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io])
+
+(defn data-io
+(with-open [reader (io/reader "in-file.csv")]
+  (doall
+    (csv/read-csv reader)))
+
+(with-open [writer (io/writer "out-file.csv")]
+  (csv/write-csv writer
+                 [["abc" "def"]
+                  ["ghi" "jkl"]]))
+)
